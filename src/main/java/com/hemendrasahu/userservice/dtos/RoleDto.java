aee1c1d3e9f5d21bccb4b324d3443f76afc97a06
hemendra <hsahu003@gmail.com>
2026-01-28 15:48:13 +0530

    added create role

---
+++ src/main/java/com/hemendrasahu/userservice/dtos/RoleDto.java
@@ -0,0 +1,12 @@
+package com.hemendrasahu.userservice.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class RoleDto {
+    private Long id;
+    private String name;
+}
--- src/main/java/com/hemendrasahu/userservice/services/RoleService.java
+++ src/main/java/com/hemendrasahu/userservice/services/RoleService.java
@@ -1,4 +1,27 @@
 package com.hemendrasahu.userservice.services;
 
+import com.hemendrasahu.userservice.models.Role;
+import com.hemendrasahu.userservice.repositories.RoleRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RoleService {
+
+    private final RoleRepository roleRepository;
+
+    // Use constructor injection instead of @Autowired for better testability
+    public RoleService(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    public Role createRole(String name) {
+        Role role = new Role();
+        role.setName(name);
+        return roleRepository.save(role);
+    }
+
+    public Role getRoleById(Long id) {
+        return roleRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Role not found with id: " + id));
+    }
 }
